d231800139b7883479194505561b243752c2374a
Qobiljon <qobiljon.toshnazarov@gmail.com>
2025-11-10 11:56:59 +0500

    updating sort algorithm logic

--- src/sort-lib.ts
+++ src/sort-lib.ts
@@ -4,5 +4,6 @@
 export function sortProducts(
   items: Array<{ name: string; stars: number }>
 ): Array<{ name: string; stars: number }> {
-  return items.sort((a, b) => a.name.localeCompare(b.name));
+  // return items.sort((a, b) => a.name.localeCompare(b.name));
+  return items.splice(0);
 }
